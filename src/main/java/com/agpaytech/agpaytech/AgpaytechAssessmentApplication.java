488ee6172797a9e1fa35562dac63b9de7edfee55
olu-damilare <djolayemi@gmail.com>
2022-06-20 00:00:08 +0100

    main app

---
+++ src/main/java/com/agpaytech/agpaytech/AgpaytechAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.agpaytech.agpaytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgpaytechAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgpaytechAssessmentApplication.class, args);
+	}
+
+}
