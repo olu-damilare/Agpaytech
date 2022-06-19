6ec129d8d9ed68a69835e6d56912f1482b7836bb
olu-damilare <djolayemi@gmail.com>
2022-06-20 00:00:56 +0100

    repo

---
+++ src/main/java/com/agpaytech/agpaytech/repository/CountryRepository.java
@@ -0,0 +1,12 @@
+package com.agpaytech.agpaytech.repository;
+
+import com.agpaytech.agpaytech.models.Country;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryRepository {
+
+    public static List<Country> countryList = new ArrayList<>();
+}
